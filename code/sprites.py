c6fe5b0190ccbf0dc50b2457fa5cfc63ebab6474
Ahmed-97 <blejahmed@gmail.com>
2026-01-08 19:20:51 +0100

    implement gravity mechanism for player

--- code/sprites.py
+++ code/sprites.py
@@ -19,17 +19,23 @@ class Player(Sprite):
         self.direction = pygame.Vector2()
         self.collision_sprites = collision_sprites
         self.speed = 400
+        self.gravity = 50
+        self.on_floor = False
 
     def input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-        self.direction = self.direction.normalize() if self.direction else self.direction
+        if keys[pygame.K_SPACE] and self.on_floor:
+            self.direction.y = -20
 
     def move(self, dt):
+        # horizontal:
         self.rect.x += self.direction.x * self.speed * dt
         self.collision('horizontal')
-        self.rect.y += self.direction.y * self.speed * dt
+
+        # vertical
+        self.direction.y += self.gravity * dt
+        self.rect.y += self.direction.y
         self.collision('vertical')
 
     def collision(self, direction):
@@ -45,7 +51,14 @@ class Player(Sprite):
                         self.rect.bottom = sprite.rect.top
                     if self.direction.y < 0:
                         self.rect.top = sprite.rect.bottom
+                    self.direction.y = 0
+
+    def check_floor(self):
+        bottom_rect = pygame.FRect((0, 0), (self.rect.width, 2)).move_to(midtop=self.rect.midbottom)
+        self.on_floor = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision_sprites]) >= 0 \
+            else False
 
     def update(self, dt):
+        self.check_floor()
         self.input()
         self.move(dt)
